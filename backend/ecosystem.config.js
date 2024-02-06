ba911d3026681f1bc0e2efe927915562e5f54f87
Danbka-Taranbka <danila.klim01@gmail.com>
2024-02-06 19:00:36 +0300

    deploy bug fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
 dotenv.config({path: "./.env.deploy"});
 
 const {
@@ -22,7 +22,7 @@ module.exports = {
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
       'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && pwd npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 }
